b82f2cb360b20fb309ec26fe09ab4e7b62f7d66f
SanketUpadhyay01 <88078531+SanketUpadhyay01@users.noreply.github.com>
2024-11-15 12:48:54 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from controller.banking_controller import BankController
-
-if __name__ == "__main__":
-
-    """Entry point for program"""
-
-    controller = BankController()
-    controller.main_menu()
